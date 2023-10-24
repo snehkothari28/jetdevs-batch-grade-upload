6d9445e63c50219b0008e48cedaca7c190b4a3a4
snehkothari28 <sneh.kothari28@gmail.com>
2023-10-24 18:10:31 +0530

    Initial commit

---
+++ src/main/java/com/jetdevs/batchgradeupload/BatchGradeUploadApplication.java
@@ -0,0 +1,13 @@
+package com.jetdevs.batchgradeupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchGradeUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchGradeUploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jetdevs/batchgradeupload/BatchGradeUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetdevs.batchgradeupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchGradeUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
