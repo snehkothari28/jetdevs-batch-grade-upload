961064963078abde62fde9d71b62ff5227f4f98b
snehkothari28 <sneh.kothari28@gmail.com>
2023-10-25 02:42:38 +0530

    Added swagger dependency

--- src/main/java/com/jetdevs/batchgradeupload/config/WebSecurityConfig.java
+++ src/main/java/com/jetdevs/batchgradeupload/config/WebSecurityConfig.java
@@ -13,15 +13,28 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class WebSecurityConfig {
 
 
+    private static final String[] AUTH_WHITELIST = {
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+    };
     @Autowired
     private CustomAuthenticationManager customAuthenticationManager;
-
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(
                         (auth) -> auth
+                                .requestMatchers(AUTH_WHITELIST).permitAll()
                                 .anyRequest().authenticated()
                 )
                 .httpBasic(withDefaults())
